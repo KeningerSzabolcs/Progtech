0d2bcc4aa3ae64b86309f39ee30b4ce7a0028067
KeningerSzabolcs <keninger.szabolcs@gmail.com>
2024-10-09 15:08:21 +0200

    feladat befejezve

--- Homework/homework_1/src/main/java/HelloWord.java
+++ Homework/homework_1/src/main/java/HelloWord.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("hello word");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Add meg a neved:");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Add meg a nemzetiseged (hu/en/ger):");
+        String language = scanner.nextLine();
+
+        if (language.isEmpty()) {
+            language = "hu";
+        }
+
+        switch (language.toLowerCase()){
+            case "hu":
+                System.out.println("Hello " + name + "!");
+                break;
+            case "en":
+                System.out.println("Hello " + name + "!");
+                break;
+            case "ger":
+                System.out.println("Hallo " + name + "!");
+                break;
+
+            default:
+                System.out.println("Ismeretlen nyelv!");
+                System.out.println("szia " + name + "!");
     }
-}
+        scanner.close();
+}}
